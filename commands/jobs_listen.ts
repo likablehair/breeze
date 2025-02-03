71f6231317846194ca450f5a76fac464e3ad5769
Gabriele Garlaschelli <gabriele.garlaschelli@likablehair.it>
2025-02-03 14:15:12 +0100

    undescore on unused var

--- commands/jobs_listen.ts
+++ commands/jobs_listen.ts
@@ -97,7 +97,7 @@ export default class JobsListen extends BaseCommand {
         logger.error(error.message, [])
       })
 
-      worker.on('closing', (msg: string) => {})
+      worker.on('closing', (_msg: string) => {})
 
       worker.on('closed', () => {})
 
