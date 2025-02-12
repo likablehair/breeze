b98df8aa917eadc4fe10d50bfbc734dfaf5dc388
filippopassalacqua <filippo.passalacqua.13@gmai.com>
2025-02-12 10:58:11 +0100

    SYST-662  upgrade 1.1.3

--- providers/jobs_provider.ts
+++ providers/jobs_provider.ts
@@ -74,6 +74,7 @@ export default class JobsProvider {
 
     this.app.container.singleton('breeze', () => new BreezeManager(this.app))
     this.app.container.singleton('breeze.list', () => jobs)
+    console.log(jobs)
     this.app.container.singleton('breeze.queues', () => queues)
     Breeze.queues = queues
   }
