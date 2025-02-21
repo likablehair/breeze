f73f2417fc0d6898a6194f8b1960a0a2214b99c1
Gabriele Garlaschelli <gabriele.garlaschelli@likablehair.it>
2025-02-21 15:59:34 +0100

    fix typings

--- src/breeze.ts
+++ src/breeze.ts
@@ -56,7 +56,7 @@ export abstract class Breeze<TPayload = any, TResult = any> {
   declare static queues: any
   declare static key: string
 
-  abstract handle(payload: TPayload): Promise<TResult> | TResult
+  abstract handle(job: BullmqJob<TPayload, TResult>): Promise<TResult> | TResult
 
   static async dispatch<DataType, ResultType>(
     payload: DataType,
