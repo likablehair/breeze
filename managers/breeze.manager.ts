a96a438b0c76699c29d6129da5af9f08339482e9
filippopassalacqua <filippo.passalacqua.13@gmai.com>
2025-03-24 14:59:16 +0100

    SYST-745

--- managers/breeze.manager.ts
+++ managers/breeze.manager.ts
@@ -62,11 +62,10 @@ export class BreezeManager {
   }
 
   private run(job: Breeze, queueName: string, config: ReturnType<typeof defineConfig>): void {
-
     const workerOptions: WorkerOptions = {
       ...config.workerOptions,
       connection: config.connection,
-      concurrency: job.concurrency ?? 1
+      concurrency: job.concurrency ?? 1,
     }
     const queueEventsOptions: QueueEventsOptions = {
       connection: config.connection,
